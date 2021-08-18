697a3c60916abf1917af4ca005e4c00c1e586ee2
fLocKBR <52131752+fLocKBR@users.noreply.github.com>
2021-08-18 18:49:54 -0300

    Corrigindo apontamentos no /etc/hosts

--- provision/prereq.sh
+++ provision/prereq.sh
@@ -7,7 +7,7 @@ systemctl restart sshd
 
 cat <<EOF > /etc/hosts
 127.0.0.1 localhost.localdomain localhost
-192.168.100.10 node-primary.example.com node-primary
-192.168.100.11 node-secondary.example.com node-secondary
-192.168.100.256 monitoring.example.com monitoring
+192.168.200.10 node-primary.example.com node-primary
+192.168.200.11 node-secondary.example.com node-secondary
+192.168.200.254 monitoring.example.com monitoring
 EOF
